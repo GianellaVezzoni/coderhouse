db6c57f12ee3259a28a66f44d126d06fed382041
Gianella Vezzoni <gianellavezzoni@MacBook-Air-de-Gianella.local>
2022-10-31 09:09:10 -0300

    get by id, get all, delete by id and get all functions modified

--- clase-4/Contenedor.js
+++ clase-4/Contenedor.js
@@ -1,67 +1,110 @@
 const fs = require("fs");
 class Contenedor {
-  constructor(fileName) {
-    this.fileName = fileName;
-  }
+ constructor(fileName) {
+  this.fileName = fileName;
+ }
 
-  async save(obj) {
-    //Recibe un obj, lo guarda en el archivo y devuelve el id asignado
-    // Los ids no pueden estar repetidos
-    try {
-        let objToSave;
-      if (fs.existsSync(this.fileName)) {
-        const file = await fs.promises.readFile(this.fileName, "utf-8");
-        const fileParsed = JSON.parse(file);
-        const lastElementId = fileParsed[fileParsed.length - 1].id;
-        objToSave = {
-          ...obj,
-          id: lastElementId + 1,
-        };
-        const elements = fileParsed.push(objToSave);
-        console.log("el array ", elements)
-        await fs.promises.appendFile(
-          this.fileName,
-          JSON.stringify(elements)
-        );
-      } else {
-        objToSave = {
-          ...obj,
-          id: 1,
-        };
-        const array = [objToSave];
-        await fs.promises.writeFile(
-          this.fileName,
-          JSON.stringify(array),
-          "utf-8"
-        );
-      }
-      console.log("Objeto guardado. Id asigndo: ", objToSave.id)
-    } catch (err) {
-      console.log("en el catch ", err);
-    }
+ async save(obj) {
+  try {
+   let objToSave;
+   if (fs.existsSync(this.fileName)) {
+    const file = await fs.promises.readFile(this.fileName, "utf-8");
+    const fileParsed = JSON.parse(file);
+    const lastElementId =
+     fileParsed.length > 0 ? fileParsed[fileParsed?.length - 1].id : 1;
+    objToSave = {
+     ...obj,
+     id: lastElementId + 1,
+    };
+    fileParsed.push(objToSave);
+    await fs.promises.writeFile(
+     this.fileName,
+     JSON.stringify(fileParsed),
+     "utf-8"
+    );
+   } else {
+    objToSave = {
+     ...obj,
+     id: 1,
+    };
+    const array = [objToSave];
+    await fs.promises.writeFile(this.fileName, JSON.stringify(array), "utf-8");
+   }
+   console.log("Objeto guardado. Id asigndo: ", objToSave.id);
+  } catch (err) {
+   throw new Error("Error al guardar un elemento", err);
   }
+ }
 
-  getById(number) {
-    // Recibe un id y devuelve todo el obj de ese id o null si este no está
+ async getById(numberId) {
+  try {
+   const file = await fs.promises.readFile(this.fileName, "utf-8");
+   const elementFounded = JSON.parse(file).find(
+    (element) => element.id === numberId
+   );
+   if (elementFounded) {
+    console.log(`Elemento encontrado!.`, elementFounded);
+   } else {
+    console.log("No se encontró el elemento solicitado.");
+   }
+  } catch (err) {
+   throw new Error(`Error al buscar elementos por id. ${err}`);
   }
+ }
 
-  getAll() {
-    //Debe devolver el array con los objetos del archivo
+ async getAll() {
+  try {
+   const file = await fs.promises.readFile(this.fileName, "utf-8");
+   console.log("Elementos: ", file);
+  } catch (err) {
+   throw new Error("Error al listar todos los elementos del archivo ", err);
   }
+ }
 
-  deleteById() {
-    //Debe eliminar del archivo el obj con el id buscado
+ async deleteById(numberId) {
+  try {
+   const file = await fs.promises.readFile(this.fileName, "utf-8");
+   //Busco el elemento en el archivo
+   const elementToDelete = JSON.parse(file).find(
+    (element) => element.id === numberId
+   );
+   if (elementToDelete) {
+    //Si encuentra el elemento, filtra el archivo para obtener los que son diferentes al id solicitado
+    const elementsToSave = JSON.parse(file).filter(
+     (element) => element.id !== numberId
+    );
+    console.log("Elemento eliminado!");
+    await fs.promises.writeFile(
+     this.fileName,
+     JSON.stringify(elementsToSave),
+     "utf-8"
+    );
+   } else {
+    console.log("No se encontró el elemento solicitado.");
+   }
+  } catch (err) {
+   throw new Error("Error al eliminar un elemento por id ", err);
   }
+ }
 
-  deleteAll() {
-    // Elimina todos los obj del archivo
+ async deleteAll() {
+  try {
+   await fs.promises.writeFile(this.fileName, JSON.stringify([]), "utf-8");
+   console.log("Elementos eliminados con éxito!");
+  } catch (err) {
+   throw new Error("Error al eliminar los elementos ", err);
   }
+ }
 }
 const obj = {
-  title: "Lapices",
-  price: 200,
-  thumbnail:
-    "https://economiaparatodos.net/wp-content/uploads/LAPIZ-e1372204441533.jpg",
+ title: "Lapices",
+ price: 200,
+ thumbnail:
+  "https://economiaparatodos.net/wp-content/uploads/LAPIZ-e1372204441533.jpg",
 };
 const containerObj = new Contenedor("productos.txt");
 containerObj.save(obj);
+// containerObj.getById(parseInt(Math.random() * 10));
+// containerObj.getAll();
+// containerObj.deleteById(1);
+// containerObj.deleteAll();
