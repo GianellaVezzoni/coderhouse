a529f2146c9e2b20d0e33f7701a9e659f4837fb9
Gianella Vezzoni <vezzoni.00@gmail.com>
2022-11-14 18:40:11 -0300

    desafio 8 y clase 8 ejercicio 1

--- clase-8/desafio/products.js
+++ clase-8/desafio/products.js
@@ -1,9 +1,23 @@
-[
+module.exports = [
   {
     id: 1,
     title: "Regla",
     price: 20.2,
     thumbnail:
       "https://d1kyb25k9pj5nw.cloudfront.net/media/catalog/product/cache/bc959f0e4eaab18479332046e9efb53d/2/0/200020.jpg",
+  },
+  {
+    id: 2,
+    title: "Goma",
+    price: 10,
+    thumbnail:
+      "https://www.papelerabariloche.com.ar/img/p/163214/1.jpeg?quality=95&width=800&height=800&mode=max&upscale=false",
+  },
+  {
+    id: 3,
+    title: "Plasticola",
+    price: 60,
+    thumbnail:
+      "https://alotar.vteximg.com.br/arquivos/ids/157362-1000-1000/CBLPL40G_1.jpg?v=637372645913200000",
   },
 ];
--- clase-8/desafio/server.js
+++ clase-8/desafio/server.js
@@ -1,15 +1,149 @@
-const express = require('express');
-const {Router} = express;
+const express = require("express");
+let products = require("./products");
+const multer = require('multer');
+const { Router } = express;
 const app = express();
-
+const PORT = 8080;
 const productsRouter = new Router();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
 
-productsRouter.get('/', (req, res) => {
-    
+productsRouter.get("/", (_, res) => {
+  try {
+    res.send({
+      status: "success",
+      productos: products,
+    });
+  } catch (err) {
+    res.send({
+      status: "error",
+      err: err,
+    });
+  }
 });
 
-app.use('/api/productos', productsRouter);
+productsRouter.get("/:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    const productFounded = products.find(
+      (element) => element.id === parseInt(id)
+    );
+    if (productFounded) {
+      res.send({
+        status: "success",
+        producto: productFounded,
+      });
+    } else {
+      res.send({
+        status: "success",
+        error: `Producto no encontrado`,
+      });
+    }
+  } catch (err) {
+    res.send({
+      status: "error",
+      error: err,
+    });
+  }
+});
 
-const server = app.listen(PORT, ()=> {
-    console.log(`Servidor escuchando en el puerto ${PORT}`);
-})
+//multer config
+const storage = multer.diskStorage({
+  destination: function(req, file, next){
+    next(null, 'uploads');
+  },
+  filename: function(req, file, next){
+    next(null, `${file.originalname}`);
+  }
+});
+
+const upload = multer({storage: storage});
+//end multer config
+
+productsRouter.post("/", upload.single('productFile'), (req, res) => {
+  try{
+    const lastIdArray = products[products.length -1].id + 1;
+    const file = req.file;
+    if(!file){
+      const error = new Error('Error, no se subió ningún archivo');
+      res.send({
+        status: "error",
+        error: error,
+      });
+    }
+    const {body} = req;
+    products.push({
+      ...body,
+      id: lastIdArray,
+      thumbnail: file.path
+    });
+    res.send({
+      status: "success",
+      message: `Producto cargado con éxito. ID asignado ${lastIdArray}`,
+    });
+  }catch(err){
+    res.send({
+      status: "error",
+      error: 'Error al crear un producto',
+    });
+  }
+});
+
+productsRouter.put("/:id", (req, res) => {
+  try{
+    const {id} = req.params;
+    const {title, price, thumbnail} = req.body;
+    const productFounded = products.findIndex(element => element.id === parseInt(id));
+    if(productFounded > -1){
+      products[productFounded] = {
+        id: products[productFounded].id,
+        title: title ? title : products[productFounded].title,
+        price: price ? price : products[productFounded].price,
+        thumbnail: thumbnail ? thumbnail : products[productFounded].thumbnail
+      };
+      res.send({
+        status: "success",
+        message: 'Producto actualizado con éxito!',
+        product: products[productFounded]
+      });
+    }else{
+      res.send({
+        status: "error",
+        error: `Error, no se encontró un producto con id ${id}`
+      });
+    }
+  }catch(err){
+    res.send({
+      status: "error",
+      error: err,
+    });
+  }
+});
+
+productsRouter.delete("/:id", (req, res) => {
+  try{
+    const {id} = req.params;
+    const productsFiltered = products.filter(element => element.id !== parseInt(id));
+    products = productsFiltered;
+    res.send({
+      status: "success",
+      message: 'Producto eliminado con éxito!',
+      product: products
+    });
+  }catch(err){
+    res.send({
+      status: "error",
+      error: 'Error al eliminar un producto',
+    });
+  }
+});
+
+app.use("/api/productos", productsRouter);
+
+const server = app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
+server.on("error", (err) =>
+  console.log(`Error al ejecutar el servidor ${err}`)
+);
