604c1377e3d08b351ed3d52981af74bfc901b952
Gianella Vezzoni <gianellavezzoni@MacBook-Air-de-Gianella.local>
2022-12-04 14:09:52 -0300

    products routes and functions CRUD implemented

--- proyecto-final/src/persistence/productContainer.js
+++ proyecto-final/src/persistence/productContainer.js
@@ -1,24 +1,141 @@
 const fs = require("fs");
 
 module.exports = class ProductContainer {
-  constructor(fileName) {
-    this.fileName = fileName;
-  }
+ constructor(fileName) {
+  this.fileName = fileName;
+ }
 
-  async getProducts() {
-    try {
-      if (fs.existsSync(this.fileName)) {
-        console.log('en el if')
-        const file = await fs.promises.readFile(this.fileName, "utf-8");
-        return JSON.parse(file);
-      } else {
-        await fs.promises.writeFile(this.fileName, JSON.stringify([]), "utf-8");
-        return [];
-      }
-    } catch (err) {
-      return [];
-    }
+ async getProducts() {
+  try {
+   if (fs.existsSync(this.fileName)) {
+    const file = await fs.promises.readFile(this.fileName, "utf-8");
+    return JSON.parse(file);
+   } else {
+    await fs.promises.writeFile(this.fileName, JSON.stringify([]), "utf-8");
+    return [];
+   }
+  } catch (err) {
+   throw new Error("Error al listar todos los elementos del archivo ", err);
   }
+ }
 
-  getProductsById(productId) {}
+ async getProductsById(productId) {
+  try {
+   const file = await fs.promises.readFile(this.fileName, "utf-8");
+   const elementFounded = JSON.parse(file).find(
+    (element) => element.id === parseInt(productId)
+   );
+   if (elementFounded) {
+    return elementFounded;
+   } else {
+    return null;
+   }
+  } catch (err) {
+   throw new Error(`Error al buscar elementos por id. ${err}`);
+  }
+ }
+
+ async createProduct(obj) {
+  try {
+   let objToSave;
+   if (fs.existsSync(this.fileName)) {
+    const file = await fs.promises.readFile(this.fileName, "utf-8");
+    const fileParsed = JSON.parse(file);
+    const lastElementId =
+     fileParsed.length > 0 ? fileParsed[fileParsed?.length - 1].id : 1;
+    objToSave = {
+     ...obj,
+     id: lastElementId + 1,
+    };
+    fileParsed.push(objToSave);
+    await fs.promises.writeFile(
+     this.fileName,
+     JSON.stringify(fileParsed),
+     "utf-8"
+    );
+   } else {
+    objToSave = {
+     ...obj,
+     id: 1,
+    };
+    const array = [objToSave];
+    await fs.promises.writeFile(this.fileName, JSON.stringify(array), "utf-8");
+   }
+   return objToSave;
+  } catch (err) {
+   throw new Error("Error al guardar un elemento", err);
+  }
+ }
+
+ async deleteById(productId) {
+  try {
+   const file = await fs.promises.readFile(this.fileName, "utf-8");
+   const elementToDelete = JSON.parse(file).find(
+    (element) => element.id === parseInt(productId)
+   );
+   if (elementToDelete) {
+    const elementsToSave = JSON.parse(file).filter(
+     (element) => element.id !== parseInt(productId)
+    );
+    await fs.promises.writeFile(
+     this.fileName,
+     JSON.stringify(elementsToSave),
+     "utf-8"
+    );
+   }
+   return elementToDelete;
+  } catch (err) {
+   throw new Error("Error al eliminar un elemento por id ", err);
+  }
+ }
+
+ async updateProductById(productId, productData) {
+  try {
+   const file = await fs.promises.readFile(this.fileName, "utf-8");
+   const productsParsed = JSON.parse(file);
+   const elementToUpdate = productsParsed.find(
+    (element) => element.id === parseInt(productId)
+   );
+   let product = null;
+   if (elementToUpdate) {
+    product = {
+     ...elementToUpdate,
+     timestamp: Date.now(),
+     nombre:
+      productData?.nombre !== undefined
+       ? productData.nombre
+       : elementToUpdate.nombre,
+     descripcion:
+      productData?.descripcion !== undefined
+       ? productData?.descripcion
+       : elementToUpdate.descripcion,
+     codigo:
+      productData?.codigo !== undefined
+       ? productData?.codigo
+       : elementToUpdate.codigo,
+     foto:
+      productData?.foto !== undefined
+       ? productData?.foto
+       : elementToUpdate.foto,
+     precio:
+      productData?.precio !== undefined
+       ? productData.precio
+       : elementToUpdate.precio,
+     stock:
+      productData?.stock !== undefined
+       ? productData?.stock
+       : elementToUpdate.stock,
+    };
+    productsParsed[elementToUpdate.id] = product;
+    await fs.promises.writeFile(
+     this.fileName,
+     JSON.stringify(productsParsed),
+     "utf-8"
+    );
+    return product;
+   }
+  } catch (err) {
+   throw new Error("Error al actualizar un elemento por id ", err);
+  }
+ }
 };
--- proyecto-final/src/server.js
+++ proyecto-final/src/server.js
@@ -3,6 +3,9 @@ const routes = require("./routes");
 const app = express();
 const PORT = 8080 || process.env.PORT;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use("/api/", routes);
 
 app.listen(PORT, () => {
