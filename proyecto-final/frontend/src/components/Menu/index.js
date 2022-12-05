e609ca756dc1ed6e73064bff1930863f78305c60
Gianella Vezzoni <gianellavezzoni@MacBook-Air-de-Gianella.local>
2022-12-05 03:52:56 -0300

    frontend initialized

---
+++ proyecto-final/frontend/src/components/Menu/index.js
@@ -0,0 +1,21 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import "./Menu.css";
+
+const Menu = () => {
+ return (
+  <div className="menu-container">
+   <a href="#" className="menu-item">
+    Productos
+   </a>
+   <a href="#" className="menu-item">
+    Cargar productos
+   </a>
+   <a href="#" className="menu-item">
+    Carrito
+   </a>
+  </div>
+ );
+};
+
+export default Menu;
