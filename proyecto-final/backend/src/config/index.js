0e985328993bff54ce682f23ecd73353695fb39c
Gianella Vezzoni <gianella.vezzoni@possumus.tech>
2022-12-27 12:26:49 -0300

    firebase implemented in cart controller

--- proyecto-final/backend/src/config/index.js
+++ proyecto-final/backend/src/config/index.js
@@ -1,5 +1,7 @@
 const mongo_url = "mongodb+srv://giane:CFTORukRy4qemoIg@codercluster.mlv1pwb.mongodb.net/?retryWrites=true&w=majority";
+const firebase = require('../db/coderhouse-8d2f3-firebase-adminsdk-9f17i-3b0d7bdc76.json');
 
 module.exports = {
-    mongo_url
+    mongo_url,
+    firebase
 };
